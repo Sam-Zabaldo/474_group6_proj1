b0b27a27ab297802640b2d04e2a3de12b7e8aabb
evanmac <evanmac@udel.edu>
2020-09-21 09:43:13 -0400

    Gun moves with crosshair

--- duckHunt.js
+++ duckHunt.js
@@ -8,7 +8,9 @@ var duckHuntScene = function(){
         
     }
     this.player = new player();
+    this.exampleDuck = new target();
     this.initialize();
+
 
     this.reset=function(){
         self.score=0;
@@ -22,26 +24,44 @@ var player = function(){
     this.xPos = 500;
     this.yPos = 500;
     this.ammo = 6;
+    this.width = 50;
+    this.height = 50;
     this.canShoot = true;
 
     this.setPostion=function(xPos, yPos){
         //Implement Crosshair wiggle, recoil?
+    }
+    this.updatePosition= function(xPos, yPos){
+         //Implement Crosshair wiggle, recoil?
     }
     this.fireGun = function(){
         //Handles firing gun logic
     }
 
 }
-var target = function(targetType){
+var target = function(){
     var self = this;
+    this.angle = 0;
+    this.speed = 0;
     this.xPos = 0;
     this.yPos = 0;
-    this.type = targetType;
+    this.type = "None";
 
+    this.setTargetType = function(type){
+        this.type = type;
+    }
+    this.setTrajectory = function(angle, speed){
+        this.angle = angle;
+        this.speed = speed;
+    }
     this.setPostion = function(xPos,yPos ){
-        //TODO
+        self.xPos = xPos;
+        self.yPos = yPos;
     }
-    this.updatePosition = function(direction){
-        //TODO
+    this.updatePosition = function(time){
+        var distance=self.speed*time;
+        self.yPos=self.yPos+Math.sin(self.angle)*distance;
+        self.xPos=self.xPos+Math.cos(self.angle)*distance;
     }
+   
 }

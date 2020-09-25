27c43efd90bd4a863a141786e2d3f67bc7449eb3
Stephen Moss <smoss@udel.edu>
2020-09-25 10:50:37 -0400

    controller somewhat set up

---
+++ scripts/duckHuntController.js
@@ -0,0 +1,37 @@
+
+//initializes the overall program
+var duckHuntControllerStart = function() {
+    var gameUI;
+    var altUI;
+    var advice;
+
+    $(normGameButton).click(function(){gameUI = new duckHuntUI ()});
+
+}
+
+
+//goes to menu mode
+var duckHuntMenuStart = function () {
+
+
+}
+
+//begins normal game
+var duckHuntNormStart = function () {
+
+
+}
+
+//begins alternate game if we want to do this
+var duckHuntAltStart = function () {
+
+    
+
+}
+
+//Displays instructions screen
+var duckHuntHelpStart = function () {
+
+    
+
+}
